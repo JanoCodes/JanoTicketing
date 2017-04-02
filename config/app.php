a0dc8c4024a4be8af698ed38474967239a878c27
Andrew Ying <hi@andrewying.com>
2017-04-02 09:44:45 +0800

    Add various dependencies

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        anlutro\LaravelSettings\ServiceProvider::class,
 
         /*
          * Application Service Providers...
---
+++ config/settings.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'store' => 'json',
+
+	// if the json store is used, give the full path to the .json file
+	// that the store writes to.
+	'path' => storage_path().'/settings.json'
+);
