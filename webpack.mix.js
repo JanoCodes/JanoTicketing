69850ff11e2a5a6bcbe5e9df5fd4e376b6ff5f1b
Andrew Ying <hi@andrewying.com>
2017-04-24 13:07:18 +0100

    Add Font Awesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,9 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .extract(['lodash', 'jquery', 'foundation-sites', 'vue', 'axios'])
     .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+
+if (mix.config.inProduction) {
+    mix.version();
+}
