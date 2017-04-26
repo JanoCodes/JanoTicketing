9fd68af79987cd6baef2e0c39affe2b7db825eff
Andrew Ying <hi@andrewying.com>
2017-04-26 22:18:08 +0100

    Correct typo in view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="row columns">
-        <h2>{{ Settings::get('system.title.' . App::getLocale()) }}</h2>
-        {{ __('system.welcome', ['title' => Settings::get('system.title.' . App::getLocale())]) }}
+        <h2>{{ Setting::get('system.title.' . App::getLocale()) }}</h2>
+        {{ __('system.welcome', ['title' => Setting::get('system.title.' . App::getLocale())]) }}
         <div class="button-group stacked-for-small hollow large">
-            @if (Settings::get('system.registration.method.oauth.active'))
+            @if (Setting::get('system.registration.method.oauth.active'))
                 <a class="button" href="{{ route('oauth') }}">
-                    {{ Settings::get('system.registration.method.oauth.name.' . App::getLocale()) }}
+                    {{ Setting::get('system.registration.method.oauth.name.' . App::getLocale()) }}
                 </a>
             @endif
             @if (Settings::get('system.registration.method.generic.active'))
                 <a class="button" href="{{ route('login') }}">
-                    {{ Settings::get('system.registration.method.generic.name.' . App::getLocale()) }}
+                    {{ Setting::get('system.registration.method.generic.name.' . App::getLocale()) }}
                 </a>
             @endif
         </div>
