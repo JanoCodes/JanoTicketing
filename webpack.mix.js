e70cb72ac923814834a159a01a485d848eb9121d
Andrew Ying <hi@andrewying.com>
2017-05-07 18:34:23 +0100

    Allow customisation of email CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,10 @@
 const { mix } = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/assets/js/app.js', 'public/js')
     .extract(['lodash', 'jquery', 'foundation-sites', 'vue', 'axios'])
     .sass('resources/assets/sass/app.scss', 'public/css');
 
+mix.copy('logo.png', 'public/images/logo.png');
 mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
 
 if (mix.config.inProduction) {
