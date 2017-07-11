c5e546b10979b97358d446b7d534e0da2e4b1e35
Andrew Ying <hi@andrewying.com>
2017-07-11 16:24:07 +0100

    Update Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .extract(['lodash', 'jquery', 'foundation-sites', 'vue', 'axios'])
+    .extract(['lodash', 'jquery', 'vue', 'foundation-sites', 'axios'])
     .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.copy('logo.png', 'public/images/logo.png');
