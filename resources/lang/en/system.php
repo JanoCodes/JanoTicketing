3a749626063fe11a3ebf4e501409168e836b7e82
Andrew Ying <hi@andrewying.com>
2018-12-27 19:28:23 +0800

    Fix error in integration with Lokalise

--- resources/lang/en/system.php
+++ resources/lang/en/system.php
@@ -6,17 +6,18 @@
  * This file is part of Jano Ticketing System.
  *
  * Jano Ticketing System is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License v3.0 as
- * published by the Free Software Foundation. You must preserve all legal
- * notices and author attributions present.
+ * modify it under the terms of the GNU Affero General Public License
+ * v3.0 as published by the Free Software Foundation. You must preserve
+ * all legal notices and author attributions present.
  *
  * Jano Ticketing System is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 return [
@@ -26,10 +27,7 @@ return [
      */
     'about' => 'About',
     'about_copyright' => '&copy; Andrew Ying and contributors 2016-18.',
-    'about_license' => 'Jano Ticketing System is free software: you can redistribute it and/or modify it'
-        . ' under the terms of the <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank"'
-        . '>GNU General Public License v3.0</a>. You <b>must</b> preserve all legal notices and author '
-        . 'attributions present.',
+    'about_license' => 'Jano Ticketing System is free software: you can redistribute it and/or modify it under the terms of the <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GNU General Public License v3.0</a>. You <b>must</b> preserve all legal notices and author attributions present.',
     'account' => 'Your Account',
     'accepted_file_types' => 'Accepted file type(s): :extensions',
     'access_level' => 'Access Level',
@@ -49,9 +47,7 @@ return [
     'collection_edit' => 'Edit Proxy Information',
     'confirm_password' => 'Confirm Password',
     'continue' => 'Continue',
-    'copyright' => 'Copyright &copy; Andrew Ying 2016-18. Licensed under the '
-        . '<a href="https://github.com/jano-may-ball/ticketing/blob/master/LICENSE.md" target="_blank">'
-        . 'GNU General Public License v3.0</a>.',
+    'copyright' => 'Copyright &copy; Andrew Ying 2016-18. Licensed under the <a href="https://github.com/jano-may-ball/ticketing/blob/master/LICENSE.md" target="_blank">GNU General Public License v3.0</a>.',
     'currency' => 'Currency',
     'create_group' => 'Create Group',
     'create_order' => 'Create Order',
@@ -67,8 +63,7 @@ return [
     'edit' => 'Edit Entry',
     'email' => 'Email Address',
     'exception_title' => 'Something went wrong...',
-    'exception_message' => 'It looks like someone messed with the wires and now the system stopped working. We\'ve '
-        . 'alerted an administrator and they should fix the issue very soon!',
+    'exception_message' => 'It looks like someone messed with the wires and now the system stopped working. We\'ve alerted an administrator and they should fix the issue very soon!',
     'event_date' => 'Event Date',
     'event_location' => 'Event Location',
     'event_name' => 'Event Name',
@@ -85,13 +80,11 @@ return [
     'import_entries' => 'Import Entries',
     'internal_reference' => 'Internal Reference',
     'jano_ticketing_system' => 'Jano Ticketing System',
-    'job_successfully_queued' => 'The job has been successfully queued. You will be notified when it has been'
-        . ' successfully processed.',
+    'job_successfully_queued' => 'The job has been successfully queued. You will be notified when it has been successfully processed.',
     'last_name' => 'Last Name',
     'location_name' => 'Location Name',
     'login' => 'Login',
-    'login_required' => 'Please login/register <a href="#login-modal" data-toggle="modal">here</a> to '
-        . 'purchase tickets for this event.',
+    'login_required' => 'Please login/register <a href="#login-modal" data-toggle="modal">here</a> to purchase tickets for this event.',
     'logout' => 'Logout',
     'method' => 'Method',
     'new_entry' => 'New Entry',
@@ -101,8 +94,7 @@ return [
     'no_ticket_type_exists' => 'No ticket type exists for this event.',
     'order' => 'Order',
     'order_confirmed' => 'Order Confirmed',
-    'order_confirmed_message' => 'Congratulations! Your order has been successful and you should be receiving an email'
-        . ' confirmation shortly.',
+    'order_confirmed_message' => 'Congratulations! Your order has been successful and you should be receiving an email confirmation shortly.',
     'order_summary' => 'Order Summary',
     'order_tickets' => 'Order Tickets',
     'original_attendee' => 'Original Attendee',
@@ -121,13 +113,10 @@ return [
     'primary_ticket_holder' => 'Primary ticket holder',
     'process_transfers' => 'Process pending transfers',
     'processing_order_title' => 'Processing Order',
-    'processing_order_message' => 'We are currently processing your order. Please do not refresh this window. You will '
-        . 'be redirected automatically when your order is confirmed.',
+    'processing_order_message' => 'We are currently processing your order. Please do not refresh this window. You will be redirected automatically when your order is confirmed.',
     'quantity' => 'Quantity',
     'unable_to_process_order_title' => 'Unable to Process Order',
-    'unable_to_process_order_message' => 'An unexcepted system issue prevents us from processing your order at this '
-        . 'moment. Please try again later. We apologise for the inconvenience and please feel free to contact us if you'
-        . ' require any assistance.',
+    'unable_to_process_order_message' => 'An unexcepted system issue prevents us from processing your order at this moment. Please try again later. We apologise for the inconvenience and please feel free to contact us if you require any assistance.',
     'unique_id' => 'Unique ID',
     'unique_id_regeneration_queued' => 'Unique ID regeneration queued',
     'upload_files_prompt' => 'Drag files here to upload',
@@ -138,16 +127,12 @@ return [
     'remember_me' => 'Remember Me',
     'request_create' => 'Create Waiting List Entry',
     'request_created' => 'Waiting List Entry Created',
-    'request_created_message' => 'You have successfully submitted an entry to the waiting list. The details of the '
-        . 'entry are supplied below for your reference. You would be notified via email should a ticket becomes '
-        . 'available.',
+    'request_created_message' => 'You have successfully submitted an entry to the waiting list. The details of the entry are supplied below for your reference. You would be notified via email should a ticket becomes available.',
     'request_edit' => 'Edit Waiting List Entry',
     'reservation_expiring_popup' => 'Reservation Expiring',
     'reservation_expiring_title' => 'Ticket Reservation Expiring',
-    'reservation_expiring_message' => 'Your ticket reservation is expiring in the next few minutes. Please complete '
-        . 'the ticket order as soon as possible. Otherwise the tickets reserved will be released.',
-    'reservation_expired' => '<strong>Ticket Reservation Expired</strong><br />The ticket reservation has expired and '
-        . 'the tickets have been released. Please try again if you still want tickets for the event.',
+    'reservation_expiring_message' => 'Your ticket reservation is expiring in the next few minutes. Please complete the ticket order as soon as possible. Otherwise the tickets reserved will be released.',
+    'reservation_expired' => '<strong>Ticket Reservation Expired</strong><br />The ticket reservation has expired and the tickets have been released. Please try again if you still want tickets for the event.',
     'reset' => 'Reset',
     'reset_password' => 'Reset Password',
     'right_to_buy' => 'Right to Buy',
@@ -168,8 +153,7 @@ return [
     'total_charges' => 'Total Charges',
     'ticket' => 'Ticket',
     'ticket_limit' => 'Ticket Limit',
-    'ticket_limit_exceeded' => 'You have chosen a number of tickets exceeding the number of tickets'
-        . ' you are allowed to purchase.',
+    'ticket_limit_exceeded' => 'You have chosen a number of tickets exceeding the number of tickets you are allowed to purchase.',
     'ticket_order_begins' => 'Ticket Order Begins',
     'ticket_order_for_attendee' => 'Ticket order for :count attendee|Ticket order for :count attendees',
     'ticket_orders' => 'Ticket Orders',
@@ -182,11 +166,9 @@ return [
     'tickets_unavailable_message' => 'All tickets are currently reserved. Tickets may be released if other patrons fail'
         . ' to complete their transactions within 15 minutes. Please try again later.',
     'tickets_partly_unavailable_title' => 'Unable to reserve all tickets requested',
-    'tickets_partly_unavailable_message' => 'Please check the order summary to the right for the number of tickets the '
-        . 'system was able to reserve for you, and either continue with or cancel your order as appropriate.',
+    'tickets_partly_unavailable_message' => 'Please check the order summary to the right for the number of tickets the system was able to reserve for you, and either continue with or cancel your order as appropriate.',
     'tickets_reserved_title' => 'Relax - the tickets have been held for you!',
-    'tickets_reserved_message' => 'You have {{ props.minutes }} minutes and {{ props.seconds }} seconds '
-        . 'to complete your reservation.',
+    'tickets_reserved_message' => 'You have {{ props.minutes }} minutes and {{ props.seconds }} seconds to complete your reservation.',
     'time_for_payment' => 'Time for Payment',
     'title' => 'Title',
     'titles' => [
@@ -201,9 +183,7 @@ return [
     'transfer_cancel' => 'Cancel Transfer Request',
     'transfer_create' => 'Create Transfer Request',
     'transfer_created' => 'Transfer Request Created',
-    'transfer_created_details' => 'Your ticket transfer request have now been created. You should receive an email '
-        . 'with information about the next step soon. Please ensure that the instructions are being followed as '
-        . 'we cannot otherwise process your request.',
+    'transfer_created_details' => 'Your ticket transfer request have now been created. You should receive an email with information about the next step soon. Please ensure that the instructions are being followed as we cannot otherwise process your request.',
     'transfer_edit' => 'Edit Transfer Request',
     'type' => 'Ticket Type',
     'types' => 'Ticket Types',
@@ -211,19 +191,16 @@ return [
     'update_primary_ticket_holder' => 'Update Primary Ticket Holder',
     'update_success' => 'Entry updated successfully',
     'upload' => 'Upload',
-    'upload_column_match_message' => 'We were unable to automatically identify the column headings for {{ name }}...'
-        . ' Please do the matching manually below.',
+    'upload_column_match_message' => 'We were unable to automatically identify the column headings for {{ name }}... Please do the matching manually below.',
     'upload_error_message' => 'File upload failed',
     'upload_info_required_message' => 'Information required',
     'upload_manual_column_match_error' => 'The matching of column headings is invalid. Please try again.',
-    'upload_success_message' => 'All files have been successfully uploaded and queued for processing! You would '
-        . 'be notified when the processing is completed.',
+    'upload_success_message' => 'All files have been successfully uploaded and queued for processing! You would be notified when the processing is completed.',
     'user' => 'User',
     'users' => 'Users',
     'view_details' => 'View Details',
     'waiting_list' => 'Waiting List Entry',
     'welcome' => 'Welcome',
-    'welcome_message' => 'Welcome to :title! To purchase your event tickets and view the status of your orders, '
-        . 'please either register or login.',
+    'welcome_message' => 'Welcome to :title! To purchase your event tickets and view the status of your orders, please either register or login.',
     'your_details' => 'Your Details',
 ];
