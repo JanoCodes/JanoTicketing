0aac2ad1f439e2568585068b4741950fab8deafc
Andrew Ying <hi@andrewying.com>
2019-01-03 12:35:35 +0000

    Add coverage upload

--- .codeship/phpunit.sh
+++ .codeship/phpunit.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 supervisord -c /etc/supervisor/conf.d/supervisord.conf &
 sleep 20
-/var/www/jano/vendor/bin/phpunit
+/var/www/jano/vendor/bin/phpunit --coverage-clover
+/var/www/jano/vendor/bin/codacycoverage clover /var/www/jano/build/coverage/xml
