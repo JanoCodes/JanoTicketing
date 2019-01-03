ae3f2cc055cbfdc419591ed1a65772c0626dfcfb
Andrew Ying <hi@andrewying.com>
2019-01-03 12:45:38 +0000

    Define explicitly path for coverage file

--- .codeship/phpunit.sh
+++ .codeship/phpunit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 supervisord -c /etc/supervisor/conf.d/supervisord.conf &
 sleep 20
-/var/www/jano/vendor/bin/phpunit --coverage-clover
-/var/www/jano/vendor/bin/codacycoverage clover /var/www/jano/build/coverage/xml
+/var/www/jano/vendor/bin/phpunit --coverage-clover /var/www/jano/reports/phpunit.xml
+/var/www/jano/vendor/bin/codacycoverage clover /var/www/jano/reports/phpunit.xml
