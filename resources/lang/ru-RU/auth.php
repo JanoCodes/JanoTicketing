c9e7c7108449175d72ce364797052be7a43f0d02
Andrew Ying <hi@andrewying.com>
2019-02-08 16:08:20 +0000

    New translations auth.php (Russian)

---
+++ resources/lang/ru-RU/auth.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Jano Ticketing System
+ * Copyright (C) 2016-2017 Andrew Ying
+ *
+ * This file is part of Jano Ticketing System.
+ *
+ * Jano Ticketing System is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v3.0 as
+ * published by the Free Software Foundation.
+ *
+ * Jano Ticketing System is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Эти учетные данные не соответствуют нашим записям.',
+    'throttle' => 'Слишком много попыток входа в систему. Повторите попытку через секунд секунд.',
+
+];
