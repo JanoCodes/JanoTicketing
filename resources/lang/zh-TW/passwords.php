b2c3c6acd2716f454557123dfeb0a137daae7183
Andrew Ying <hi@andrewying.com>
2019-03-09 20:15:06 +0000

    New translations passwords.php (Chinese Traditional) [ci skip]

--- resources/lang/zh-TW/passwords.php
+++ resources/lang/zh-TW/passwords.php
@@ -1,21 +1,23 @@
 <?php
 /**
  * Jano Ticketing System
- * Copyright (C) 2016-2017 Andrew Ying
+ * Copyright (C) 2016-2019 Andrew Ying and other contributors.
  *
  * This file is part of Jano Ticketing System.
  *
  * Jano Ticketing System is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License v3.0 as
- * published by the Free Software Foundation.
+ * modify it under the terms of the GNU Affero General Public License
+ * v3.0 supplemented by additional permissions and terms as published at
+ * COPYING.md.
  *
  * Jano Ticketing System is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 return [
@@ -31,7 +33,7 @@ return [
     |
     */
 
-    'password' => '密碼必須至少為6個字元且符合確認。',
+    'password' => 'Passwords must be at least eight characters and match the confirmation.',
     'reset' => '您的密碼已重設！',
     'sent' => '我們已通過電子郵件發送了密碼重置鏈接!',
     'token' => '此密碼重設記號無效。',
